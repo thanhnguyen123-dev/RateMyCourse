4ee7c6011fa862f5983f6a486b68c33482b7c0cf
Le Thanh Nguyen <u7594144@anu.edu.au>
2025-03-02 00:20:17 +1100

    added a router to avoid caller error

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { helloRouter } from "~/server/api/routers/hello";
 
 /**
  * This is the primary router for your server.
@@ -6,6 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  hello: helloRouter,
 });
 
 // export type definition of API
