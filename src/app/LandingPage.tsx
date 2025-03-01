b38f5e1dfb1463a839d291f926c79f062d3bac3c
Le Thanh Nguyen <u7594144@anu.edu.au>
2025-03-01 20:52:15 +1100

    setup project

---
+++ src/app/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className="flex flex-col h-screen w-screen items-center justify-center">
+      <h1>Rate My Course</h1>
+      <p className="text-sm text-gray-500">coming soon...</p>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { api, HydrateClient } from "~/trpc/server";
+import LandingPage from "./LandingPage";
+import { useSession } from "next-auth/react";
+
+
+export default async function Home() {
+
+
+  return (
+    <HydrateClient>
+      <LandingPage />
+    </HydrateClient>
+  );
+}
