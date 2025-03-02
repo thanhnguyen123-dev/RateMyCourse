a34b8700e19d668702fb17a169b8e74e8e9513f0
Le Thanh Nguyen <u7594144@anu.edu.au>
2025-03-02 17:38:32 +1100

    added gradient colour to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import NavBar from "./_components/NavBar";
 export default async function Home() {
   return (
     <HydrateClient >
-      <div className="flex flex-col w-full max-w-10xl h-screen ">
+      <div className="flex flex-col w-full max-w-10xl h-screen bg-gradient-to-br from-orange-50 via-white to-orange-100">
         <NavBar />
         <div className="h-screen max-w-10xl flex flex-grow overflow-y-auto overflow-x-auto">
           <HomeScreen />
